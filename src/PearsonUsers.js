06df3b68885a12f29159235c7c4df9e5fa36eab5
Mike Williams <mike@123789.org>
2018-06-16 14:15:25 +0530

    Added check to remove duplicate users before updating state

--- src/PearsonUsers.js
+++ src/PearsonUsers.js
@@ -26,6 +26,13 @@ export class PearsonUsers extends Component {
           last_name: "Ramos",
           avatar:
             "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
+        },
+        {
+          id: 6,
+          first_name: "Tracey",
+          last_name: "Ramos",
+          avatar:
+            "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
         }
       ]
     };
@@ -33,16 +40,17 @@ export class PearsonUsers extends Component {
 
   componentDidMount(){
     // fetch users from the api endpoint for showing in the listing
-    fetch('https://reqres.in/api/users?page=1&per_page=10').
-    then( response => response.json() ).
-    then( data => {
+    fetch('https://reqres.in/api/users?page=1&per_page=10')
+    .then( response => response.json() )
+    .then( data => {
       const curUsersList = this.state.users;
       // append received users to the existing users' list
       const updatedUsersList = [...curUsersList, ...data.data];
+      const uniqueUsersList = this.removeDuplicates( updatedUsersList );
 
       // update users' list in the state
       this.setState({
-        users: updatedUsersList
+        users: uniqueUsersList
       });
     }).catch( err => {
       console.error( err );
@@ -56,14 +64,33 @@ export class PearsonUsers extends Component {
         <ul id="users-listing">
           {this.state.users.map( (user, index) => {
             return (
-              <li key={index}>
+              <li key={user.id}>
                 <img src={user.avatar} alt={user.first_name + ' ' + user.last_name} width="80" height="80" />
                 <label>{user.first_name + ' ' + user.last_name}</label>
+                <p>{user.id}</p>
               </li>
             );
           })}
         </ul>
       </div>
     );
-  }
+  }//render()
+
+  removeDuplicates( users ){
+    // this function removes duplicates users from given users' list
+    // duplicates are counted based on id being same
+
+    const idsArray =[];
+    const uniqueUsersList = [];
+    users.forEach( user => {
+      const curId = user.id
+
+      // pushed to unique users' list if this name wasn't encountered before
+      if( ! idsArray.includes( curId ) ){
+        idsArray.push( curId );
+        uniqueUsersList.push( user );
+      }
+    });//forEach()
+    return uniqueUsersList;
+  }//removeDuplicates()
 }
